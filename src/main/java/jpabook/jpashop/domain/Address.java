1efde049043adc3803be8eea53c4edb5d96b5424
DavidJeonKr <dudgh7410@naver.com>
2022-11-30 00:10:14 +0900

    세션 2. 도메인 분석 설계 => 엔티티 클래스 개발 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -3,15 +3,19 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-@Setter
+// 임베디드 타입, 값 타입은 값을 변경하지 않아야 한다.
+@ToString
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {}
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,6 +25,10 @@ public abstract class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "itmes")
+    private List<Category> categories = new ArrayList<>();
+
 
 
 }
